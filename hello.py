c0f4d2e0f51f73090282384fe4777419b1718d0c
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2017-09-14 17:11:16 +0900

    Add text parse api endpoint

--- hello.py
+++ hello.py
@@ -1,9 +1,35 @@
-from flask import Flask                           
-app = Flask(__name__)                             
+from flask import Flask, request
+import MeCab
+import json
 
-@app.route('/')                                   
-def hello_world():                                
-    return "Hello World!"                         
+app = Flask(__name__)
 
-if __name__ == '__main__':                        
+
+def parse_text(text):
+    tagger = MeCab.Tagger("-Owakati")
+    result = tagger.parse(text)
+    print('result=', result)
+    return result.split(' ')
+
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+
+@app.route('/parse', methods=['GET', 'POST'])
+def parse():
+    if request.method == 'GET':
+        text = request.args.get('text')
+    if request.method == 'POST':
+        text = request.form['text']
+
+    print('text=', text)
+    if text:
+        words = parse_text(text.encode('utf-8'))
+        return json.dumps({'result': words})
+    return json.dumps({'error': 'no text'})
+
+
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
